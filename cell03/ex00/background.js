afae7785632d0dba41255a0b1970846c96f840f4
noraainuse <nxx4235@gmail.com>
2026-09-17 10:09:13 +0700

    cell03 - ex00 button

---
+++ cell03/ex00/background.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    return randomColor;
+}
